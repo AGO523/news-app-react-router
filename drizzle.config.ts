a6f51bf62f245888bd0a186f7c46af29ae71aa20
AGO523 <unpouletfrais0523@gmail.com>
2025-04-28 08:21:23 +0900

    add: d1 settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: "06272e4c-ff19-49c4-b6c5-bdc5680135e3",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
